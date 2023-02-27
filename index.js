71519d635b09f2c90c75eb437dbe44a1d20142ce
pengju <pengjuandu@gmail.com>
2023-02-27 10:45:17 +0800

    f

---
+++ index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+    const p = ctx.request.query;
+    if (!p) {
+        p.name = '代币'
+    }
+    ctx.body = `Hello world\n你好世界\nfuck you world\n你好${p.name}`
+})
+
+app.listen(3399)
