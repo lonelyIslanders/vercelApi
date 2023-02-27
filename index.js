a3ed265f7a43ba9a66a8ccc58412dffeb4ef0255
pengju <pengjuandu@gmail.com>
2023-02-27 10:55:32 +0800

    p

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = new Koa();
 app.use(async ctx => {
     const p = ctx.request.query;
     if (!p) {
+        const p = new Object();
         p.name = '代币'
     }
     ctx.body = `Hello world\n你好世界\nfuck you world\n你好${p.name}`
