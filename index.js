11560692849e42d8b97aad4c1c3d76579e0c81bc
pengju <pengjuandu@gmail.com>
2023-02-27 11:09:40 +0800

    推

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-    const p = ctx.request.query;
-    if (!p) {
-        const p = new Object();
-        p.name = '代币'
+    let name = ctx.request.query.name;
+    if (name === undefined) {
+        name = '代币'
     }
-    ctx.body = `Hello world\n你好世界\nfuck you world\n你好${p.name}`
+    ctx.body = `Hello world\n你好世界\nfuck you world\n你好${name}`
 })
 
 app.listen(3399)
