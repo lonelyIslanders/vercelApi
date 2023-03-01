c19f46cbe6e17afb6c5966ddb0fc5581f3b4dafd
pengju <pengjuandu@gmail.com>
2023-03-01 15:43:08 +0800

    测试

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
-const dbCommand = require('./db/dbCommand');
 
 app.use(async ctx => {
     let name = ctx.request.query.name;
     if (name === undefined) {
         name = '代币'
     }
-    // const aa = await dbCommand.test()
     ctx.body = `Hello world\n你好世界\nfuck you world\n你好${name}\n`
 })
 
