05e2497fbe223782522282788c231f023aa98530
pengju <pengjuandu@gmail.com>
2023-03-01 16:07:42 +0800

    sss

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const dbCommand = require('./db/dbCommand')
 app.use(async ctx => {
     await dbCommand.tryW();
     const dd = await dbCommand.test();
-    const i = Math.floor(Math.random(dd.length));
-    let names = dd[i].name;
-    ctx.body = `Hello world\n你好世界\nfuck you world\n你好${names}\n${JSON.stringify(dd)}`
+    const len = dd.length;
+    let names = dd[0].name;
+    ctx.body = `Hello world\n你好世界\nfuck you world\n你好${names}\n${len}\n${JSON.stringify(dd)}`
 })
 
 app.listen(3399)
