433093f00435dc0ef033d6c43cc4f06f0a76345f
Islander <73678509+lonelyIslanders@users.noreply.github.com>
2023-04-17 11:49:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const Koa = require('koa');
 const app = new Koa();
-const dbCommand = require('./db/dbCommand')
 
 app.use(async ctx => {
-    await dbCommand.tryW();
-    const dd = await dbCommand.test();
-    const len = dd.length;
-    const i = Math.floor(Math.random() * len)
-    let names = dd[i].name;
-    ctx.body = `Hello world\n你好世界\nfuck you world\n你好${names}\n\n${JSON.stringify(dd)}`
+    let time=Date.now();
+    ctx.body = `Hello world\n${time}`
 })
 
 app.listen(3399)
